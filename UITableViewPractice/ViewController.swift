5523808064c0eb4298d907e1194942531e73ae18
Luke <wuchiwei17@gmail.com>
2019-07-21 00:35:08 +0800

    1. Add Custom Cell Class ProfileTableViewCell 2. Update user data depends on different section.

--- UITableViewPractice/ViewController.swift
+++ UITableViewPractice/ViewController.swift
@@ -27,17 +27,48 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     //MARK: - UITableViewDataSource
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 10
+        return 1
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "NTU", for: indexPath)
         
-        return cell
+        guard let profileCell = cell as? ProfileTableViewCell else { return cell }
+        
+        if indexPath.section == 0 {
+        
+            profileCell.imageView?.image = UIImage(named: "Steven_Jobs")
+            
+            profileCell.userNameLabel.text = "Steven_Jobs"
+            
+            profileCell.userQuoteLabel.text = "Stay Hungry, Stay Foolish."
+            
+        } else if indexPath.section == 1 {
+        
+            profileCell.imageView?.image = UIImage(named: "Tim_Cook")
+            
+            profileCell.userNameLabel.text = "Tim_Cook"
+            
+            profileCell.userQuoteLabel.text = "Make Apple Great Again!"
+        }
+        
+        return profileCell
     }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        return "Section \(section) !"
+    }
+    
+    //MARK: - UITableViewDelegatez
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         
