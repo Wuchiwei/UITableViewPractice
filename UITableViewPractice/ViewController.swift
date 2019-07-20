b49c0ba794cf09a0c959643996ad780ba1f57ced
Luke <wuchiwei17@gmail.com>
2019-07-21 00:21:14 +0800

    1. Add UITableView with user cell. 2. Connect UITableView dataSource and delegate.

--- UITableViewPractice/ViewController.swift
+++ UITableViewPractice/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupTableView()
+    }
+    
+    func setupTableView() {
+        
+        tableView.dataSource = self
+        
+        tableView.delegate = self
     }
 
+    //MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 10
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NTU", for: indexPath)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 64.0
+    }
 }
 
